a4f38c00853d304932f8894785bda3bf44a9acfa
gh-actions <actions@github.com>
2026-02-08 15:28:46 +0000

    Deploy website - based on 2a907696d4663c68c1053d709f9e25113ecdb801

--- assets/js/c15d9823.2c288d06.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksciotta_blog=self.webpackChunksciotta_blog||[]).push([[8146],{9328:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":7,"blogDescription":"Blog de tecnologia e programa\xe7\xe3o","blogTitle":"Blog Sciotta"}}')}}]);
