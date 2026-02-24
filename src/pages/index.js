6836b7cf51a4bbbdafd3353a8b877bdce3a3c92c
Ubuntu <ubuntu@ip-172-31-25-223.us-east-2.compute.internal>
2026-02-24 19:57:18 +0000

    fix: dark mode support for homepage - inverted logo + text contrast

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './index.css';
 import SciottaLogoUrl from '@site/static/img/sciotta.png';
+import SciottaDarkLogoUrl from '@site/static/img/sciotta-dark.png';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+
+function useLogo() {
+  const isBrowser = useIsBrowser();
+  if (!isBrowser) return SciottaLogoUrl;
+  const isDark = document.documentElement.getAttribute('data-theme') === 'dark';
+  return isDark ? SciottaDarkLogoUrl : SciottaLogoUrl;
+}
 
 export default function Hello() {
+  const logoUrl = useLogo();
+
   return (
     
       <div className="home">
         <section>
           <article>
-            <img src={SciottaLogoUrl} alt="Sciotta logo" />
+            <img src={logoUrl} alt="Sciotta logo" />
             <p>
-              Hi, my name is Thiago Sciotta and I’m a software developer.
+              Hi, my name is Thiago Sciotta and I'm a software developer.
             </p>
             <p>
               Try to read my <a href="/blog">blog</a>, check my <a href="/docs/intro">personal wiki</a> or follow me on social media.
